57b561c7fc04583b89996b1fefb7ae69110cd781
Eske <eskehoy@gmail.com>
2018-02-24 18:39:13 +0100

    Added new font. Added scoremanager.

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+    public int Player1Score
+    {
+        get { return player1Score; }
+        private set
+        {
+            player1Score = value;
+            Player1Text.text = player1Score.ToString();
+        }
+    }
+
+    public int Player2Score
+    {
+        get { return player2Score; }
+        private set
+        {
+            player2Score = value;
+            Player2Text.text = player2Score.ToString();
+        }
+    }
+
+    public Text Player1Text;
+    public Text Player2Text;
+
+    private int player1Score;
+    private int player2Score;
+
+
+    void Start ()
+	{
+	    Player1Score = 0;
+	    Player1Score = 0;
+	}
+
+    public void IncreasePlayer1Score()
+    {
+        Player1Score++;
+    }
+
+    public void IncreasePlayer2Score()
+    {
+        Player2Score++;
+    }
+}
