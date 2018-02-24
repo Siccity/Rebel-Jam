6320da2c55d335ef77ce9f77308e1429b823a2c8
Thor Brigsted <thorbrigsted@gmail.com>
2018-02-24 23:07:31 +0100

    Unflipped shadows (again)

--- Assets/Scripts/CameraLightSwitcher.cs
+++ Assets/Scripts/CameraLightSwitcher.cs
@@ -1,11 +1,25 @@
-﻿using UnityEngine;
-
+﻿using System.Linq;
+using UnityEngine;
 public class CameraLightSwitcher : MonoBehaviour {
 
-	public GameObject enableSun, disableSun;
+	private Light dLight {
+		get {
+			if (_dLight == null) {
+				_dLight = FindObjectsOfType<Light>().First(x => x.type == LightType.Directional);
+				dir = _dLight.transform.rotation;
+				flippedDir = Quaternion.LookRotation(Vector3.Reflect(_dLight.transform.forward, Vector3.forward));
+			}
+			return _dLight;
+		}
+	}
+	private Light _dLight;
+
+	public bool flipped;
+	private static Quaternion dir, flippedDir;
 
 	void OnPreRender() {
-		enableSun.SetActive(true);
-		disableSun.SetActive(false);
+		if (flipped) dLight.transform.rotation = flippedDir;
+		else dLight.transform.rotation = dir;
+
 	}
 }
