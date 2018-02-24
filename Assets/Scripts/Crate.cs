0cc36fd32373daebd02033f2cd61047d75058389
Eske <eskehoy@gmail.com>
2018-02-24 12:47:08 +0100

    Added scene with flipped camera. Updated crate to work with new camera setup.

--- Assets/Scripts/Crate.cs
+++ Assets/Scripts/Crate.cs
@@ -3,18 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Crate : MonoBehaviour {
-
-    //public GameObject CloneCrate;
-    private Vector3 previousPos;
-    private Vector3 previousClonePos;
-
     public CharacterController Controller;
     public CharacterController CloneController;
 
 
 	void Start () {
-        previousPos = transform.position;
-        //previousClonePos = CloneCrate.transform.position;
+
 	}
 	
 	void FixedUpdate () 
@@ -22,16 +16,11 @@ public class Crate : MonoBehaviour {
         var moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 	    Controller.Move(moveDirection*Time.deltaTime);
 	    CloneController.Move(moveDirection*Time.deltaTime);
-
-        /*
-        if (previousClonePos != CloneCrate.transform.position)
-        {
-            var diff = CloneCrate.transform.position - previousClonePos;
-            transform.Translate(diff);
-        }
-        */
-
-        previousPos = transform.position;
-        //previousClonePos = CloneCrate.transform.position;
 	}
+
+    public void Move(Vector3 movement)
+    {
+        Controller.Move(movement*Time.deltaTime);
+        CloneController.Move(movement*Time.deltaTime);
+    }
 }
