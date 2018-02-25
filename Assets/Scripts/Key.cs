3f62a11eb21102674acf93cff98500b94197324f
Flintar <a.nyborg@hotmail.com>
2018-02-25 11:37:50 +0100

    Fixed key, gatePortal and character reset.

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -7,8 +7,17 @@ public class Key : MonoBehaviour
     public bool Player1Access;
     public bool Player2Access;
 
+    private Vector3 KeyPosition;
+    void Awake(){
+        KeyPosition = transform.position;
+    }
+
     public bool HasAccess(int ID)
     {
         return ID == 1 ? Player1Access : (ID == 2 && Player2Access);
+    }
+
+    public void ResetKeyPosition(){
+        transform.position = KeyPosition;
     }
 }
